dd5a8918404127978cd1829721505afb5fb8f33a
Big-Shrine <119871441+Big-Shrine@users.noreply.github.com>
2023-09-30 14:53:54 +0530

    Update and rename Main.py to functions.py
    working of code confirmed

--- functions.py
+++ functions.py
@@ -1,8 +1,7 @@
 import mysql.connector as mys
-cobj=mys.connect('local host','root','Azsxdcfv1*','Library')
+cobj=mys.connect(host='localhost',user='root',passwd='Azsxdcfv1*',database='Library',auth_plugin='caching_sha2_password')
 curob=cobj.cursor()
 def addBook(x):
-  #x=input("Enter the number of books you want to add")
   for i in range (x):
     b=input('Enter the book id')
     c=input('Enter the genre of the book')
@@ -10,11 +9,11 @@ def addBook(x):
     e=input('Enter the author of the book')
     f=int(input('Enter the number of copies of the book'))
     if f!=0:
-      g=0
-    else:
       g=1
+    else:
+      g=0
     st="Insert into books values('{}','{}','{}','{}',{},{})".format(b,c,d,e,f,g)
     curob.execute(st)
-    curob.commit()
+    cobj.commit()
       
     
