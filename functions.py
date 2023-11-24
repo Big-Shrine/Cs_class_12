14871008e8bca6aac64be0c0016dc649890fc73d
Big-Shrine <119871441+Big-Shrine@users.noreply.github.com>
2023-11-24 08:08:09 +0530

    Update functions.py
    Changes catered to anvil

--- functions.py
+++ functions.py
@@ -1,4 +1,8 @@
+import anvil.server
 import mysql.connector as mys
+from datetime import datetime
+
+anvil.server.connect("server_ZKVLF6KO2J263VRFCP6XAVDD-7GVA2356LSL76HX5")
 cobj=mys.connect(host='localhost',user='root',passwd='Azsxdcfv1*',database='Library',auth_plugin='caching_sha2_password')
 curob=cobj.cursor()
 
@@ -6,6 +10,61 @@ curob=cobj.cursor()
 def executor(h):
   curob.execute(h)
   cobj.commit()
+  
+#function for issuing book
+@anvil.server.callable
+def issueBooks(customer, bookid, datetime_str):
+    curob.execute("select BOOK_ID, NAME, COPIES, STATUS from books")
+    books = curob.fetchall()
+    message = ''
+
+    try:
+        if datetime_str:
+            datetime_obj = datetime.strptime(datetime_str, "%Y-%m-%d %H:%M:%S")
+        else:
+            datetime_obj = None
+
+        for i in books:
+            if bookid in i:
+                l = i[2] - 1
+                if l==0:
+                    status = f"update books set COPIES={str(l)}, STATUS=0 where BOOK_ID='{i[0]}'"
+                    executor(status)
+                    message = "Book issued successfully"
+                    break
+                elif l == -1:
+                    message = 'Book unavailable'
+                    break
+                else:
+                    status = f"update books set COPIES={str(l)} where BOOK_ID='{i[0]}'"
+                    executor(status)
+                    message = "Book issued successfully"
+                    break 
+        else:
+              message = 'Invalid book ID'
+              
+        curob.execute("select customer_id,books_issued from customer_details")
+        h = curob.fetchall()
+        for i in h:
+            if customer in i:
+                issued = f"update customer_details set books_issued={str(i[1]+1)} where customer_id='{str(i[0])}'"
+                executor(issued)
+                break
+        else:
+            message = 'Invalid customer ID'
+
+              
+        if message != 'Book unavailable' and message != 'Invalid book ID' and message != 'Invalid customer ID':
+                if datetime_str:
+                    c = f"Insert into book_issues(customer_id, book_id, date_of_issue) values('{customer}','{bookid}','{datetime_str}')"
+                else:
+                    c = f"Insert into book_issues(customer_id, book_id) values('{customer}','{bookid}')"
+                executor(c)
+                
+    except ValueError:
+        message = 'Invalid datetime format. Please enter the datetime in the format YYYY-MM-DD HH:MM:SS'
+
+    return message
 
 #For adding books
 def addBook(x):
@@ -22,44 +81,6 @@ def addBook(x):
     st="Insert into books values('{}','{}','{}','{}',{},{})".format(bkid,gre,bkname,auth,copies,status)
     executor(st) 
 
-#for issuing books
-def issueBooks(y):
-  for i in range(y):
-    customer=input('Enter the customer-ID whose book has to be issued:')
-    bookid=input('Enter the ID of the book you want to add:')
-    curob.execute("select BOOK_ID, NAME, COPIES, STATUS from books")
-    books=curob.fetchall()
-    flag=False
-    for i in books:
-      if bookid in i:
-        flag=True
-        l=i[2]-1
-        if l==0:
-          status=f"update books set COPIES={str(l)}, STATUS=0 where BOOK_ID={i[0]}"
-          executor(status)
-        elif l==-1:
-          print('Book not available')
-          continue
-        else:
-          status=f"update books set COPIES={str(l)} where BOOK_ID={i[0]}"
-          executor(status)
-        datetime=input('Enter the date and time of issue in the form of YYYY-MM-DD HH:MM:SS or click enter to add current date and time:')
-        print('---------------------------------------')
-        if datetime:
-          c=f"Insert into book_issues(customer_id, book_id, date_of_issue) values({customer},{bookid},'{datetime}')"
-        else:
-          c=f"Insert into book_issues(customer_id, book_id) values({customer},{bookid})"
-        executor(c)
-        curob.execute("select customer_id,books_issued from customer_details")
-        h=curob.fetchall()
-        for i in h:
-          if customer in i:
-            issued=f"update customer_details set books_issued={str(i[1]+1)} where customer_id={i[0]}"
-            executor(issued)
-        break
-    if not flag:
-      print('Book not found')
-      print('---------------------------------------')
 
 def view_books():
   try:
