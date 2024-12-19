3f62133c41c71d20e0de38909c79318e8a9cc8ec
Big-Shrine <119871441+Big-Shrine@users.noreply.github.com>
2024-12-19 21:41:29 +0530

    Update extra.py

--- extra.py
+++ extra.py
@@ -1,23 +1,19 @@
-def remove_consecutive_3s(x):
-    x_str = str(x)
-    new_x_str = ""
-    skip_next = False
-    
-    for i in range(len(x_str) - 1):
-        if skip_next:
-            skip_next = False
-            continue
-        if x_str[i] == '3' and x_str[i + 1] == '3':
-            skip_next = True 
+def can_unlock(x):
+    while x > 0:
+
+        x_str = str(x)
+        if '33' in x_str:
+            x_str = x_str.replace('33', '')
+            x = int(x_str) if x_str else 0
         else:
-            new_x_str += x_str[i]
-    
-    
-    if not skip_next:
-        new_x_str += x_str[-1]
-    
-    
-    return int(new_x_str) if new_x_str else 0
+
+            if x >= 33:
+                x -= 33
+            else:
+                break  
+
+
+    return x == 0
 
 
 t = int(input())  
@@ -25,20 +21,7 @@ results = []
 
 for _ in range(t):
     x = int(input())
-    
-    while x > 0:
-        
-        new_x = remove_consecutive_3s(x)
-        
-        if new_x == x:  
-            if x >= 33:
-                x -= 33
-            else:
-                break  
-        else:
-            x = new_x
-    
-    if x == 0:
+    if can_unlock(x):
         results.append("YES")
     else:
         results.append("NO")
